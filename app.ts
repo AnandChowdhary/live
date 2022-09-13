599bb0e958bb86283d1768f8a5cc797631895a96
Anand Chowdhary <mail@anandchowdhary.com>
2022-09-13 23:29:45 +0200

    feat(app): Add date-wise breakdown of data

--- app.ts
+++ app.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+import dayjs from "dayjs";
 import parser from "body-parser";
 import { createHash } from "crypto";
 import express from "express";
@@ -12,13 +13,14 @@ app.use(parser.json({ limit: "100mb" }));
 const port = process.env.PORT || 3000;
 
 app.get("/", async (req, res) => {
-  const { before, after, type, value, unit } = z
+  const { before, after, type, value, unit, breakdown } = z
     .object({
       before: z.date().optional(),
       after: z.date().optional(),
       type: z.string().optional(),
       value: z.number().optional(),
       unit: z.string().optional(),
+      breakdown: z.string().optional().default("day"),
     })
     .parse({
       ...req.query,
@@ -42,19 +44,147 @@ app.get("/", async (req, res) => {
     value,
     unit,
   } as const;
-  const count = await db.healthkit_record.count({
+  const first = await db.healthkit_record.findFirst({
     where,
+    orderBy: { date: "asc" },
+  });
+  const last = await db.healthkit_record.findFirst({
+    where,
+    orderBy: { date: "desc" },
   });
   const aggregations = await db.healthkit_record.aggregate({
     where,
+    _count: { value: true },
     _avg: { value: true },
     _sum: { value: true },
     _min: { value: true },
     _max: { value: true },
   });
+  const breakdowns: [Date, Date][] = [];
+  if (first && last) {
+    if (breakdown === "day") {
+      const start = dayjs(first.date)
+        .hour(0)
+        .minute(0)
+        .second(0)
+        .millisecond(0);
+      const diff = dayjs(last.date).diff(start, "days");
+      for (let i = 0; i < diff; i++)
+        breakdowns.push([
+          start.add(i, "day").toDate(),
+          start
+            .add(i, "day")
+            .hour(23)
+            .minute(59)
+            .second(59)
+            .millisecond(9999)
+            .toDate(),
+        ]);
+    } else if (breakdown === "week") {
+      const start = dayjs(first.date)
+        .hour(0)
+        .minute(0)
+        .second(0)
+        .millisecond(0);
+      const diff = dayjs(last.date).diff(start, "weeks");
+      for (let i = 0; i < diff; i++)
+        breakdowns.push([
+          start.add(i, "week").toDate(),
+          start
+            .add(i + 1, "week")
+            .subtract(1, "day")
+            .hour(23)
+            .minute(59)
+            .second(59)
+            .millisecond(9999)
+            .toDate(),
+        ]);
+    } else if (breakdown === "month") {
+      const start = dayjs(first.date)
+        .hour(0)
+        .minute(0)
+        .second(0)
+        .millisecond(0);
+      const diff = dayjs(last.date).diff(start, "months");
+      for (let i = 0; i < diff; i++)
+        breakdowns.push([
+          start.add(i, "month").toDate(),
+          start
+            .add(i + 1, "month")
+            .subtract(1, "day")
+            .hour(23)
+            .minute(59)
+            .second(59)
+            .millisecond(9999)
+            .toDate(),
+        ]);
+    } else if (breakdown === "year") {
+      const start = dayjs(first.date)
+        .hour(0)
+        .minute(0)
+        .second(0)
+        .millisecond(0);
+      const diff = dayjs(last.date).diff(start, "years");
+      for (let i = 0; i < diff; i++)
+        breakdowns.push([
+          start.add(i, "year").toDate(),
+          start
+            .add(i + 1, "year")
+            .subtract(1, "day")
+            .hour(23)
+            .minute(59)
+            .second(59)
+            .millisecond(9999)
+            .toDate(),
+        ]);
+    } else if (breakdown === "hour") {
+      const start = dayjs(first.date).minute(0).second(0).millisecond(0);
+      const diff = dayjs(last.date).diff(start, "hours");
+      for (let i = 0; i < diff; i++)
+        breakdowns.push([
+          start.add(i, "hour").toDate(),
+          start.add(i, "hour").minute(59).second(59).millisecond(9999).toDate(),
+        ]);
+    }
+  }
+
+  const brokenDown: any[] = [];
+  for await (const { start, end, data } of asyncPool(
+    MAX_POOL_SIZE,
+    breakdowns,
+    async ([start, end]) => {
+      return {
+        start,
+        end,
+        data: await db.healthkit_record.aggregate({
+          where: { ...where, date: { gte: start, lte: end } },
+          _count: { value: true },
+          _avg: { value: true },
+          _sum: { value: true },
+          _min: { value: true },
+          _max: { value: true },
+        }),
+      };
+    }
+  )) {
+    brokenDown.push({
+      start,
+      end,
+      count: data._count.value,
+      average: data._avg.value,
+      sum: data._sum.value,
+      minimum: data._min.value,
+      maximum: data._max.value,
+    });
+  }
+
   return res.header("Cache-Control", "public, max-age=3600").json({
-    count,
-    ...aggregations,
+    count: aggregations._count.value,
+    average: aggregations._avg.value,
+    sum: aggregations._sum.value,
+    minimum: aggregations._min.value,
+    maximum: aggregations._max.value,
+    breakdown: brokenDown.sort((a, b) => a.start.getTime() - b.start.getTime()),
   });
 });
 
